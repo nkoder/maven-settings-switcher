24f58914f43ee659eb965fe784674aa1a34d3f08
Pawel Barszcz <pawelbarszcz@gmail.com>
2014-06-25 14:03:37 +0200

    show which of available settings is currently used

--- windows/mvs.ps1
+++ windows/mvs.ps1
@@ -1,12 +1,31 @@
 $SETTINGS_DIRECTORY_PATH = "$env:USERPROFILE\.m2\mvs-settings-files"
+$TARGET_SETTING_FILENAME = "settings.xml"
+$TARGET_SETTINGS_FILE_PATH = "$env:USERPROFILE\.m2\$TARGET_SETTING_FILENAME"
 
 function getListOfSettingsFiles {
-    return Get-ChildItem -path $SETTINGS_DIRECTORY_PATH
+    return Get-ChildItem -Path $SETTINGS_DIRECTORY_PATH
+}
+
+function calculateHash($filePath) {
+    $md5Provider = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $hash = $md5Provider.ComputeHash([System.IO.File]::ReadAllBytes($filePath))
+    return [System.BitConverter]::ToString($hash)
+}
+
+function isGivenFileEqualToTargetSettingsFile($filePath) {
+    $hashOfGivenFile = calculateHash $filePath
+    $hashOfTargetSettingsFile = calculateHash $TARGET_SETTINGS_FILE_PATH
+    return $hashOfGivenFile -eq $hashOfTargetSettingsFile
 }
 
 function printSettingsFilenameWithoutExtension($filename) {
     $filenamWithoutExtension = $filename.Name -replace ".xml$", ""
-    Write-Host $filenamWithoutExtension -foregroundcolor "Gray"
+    if (isGivenFileEqualToTargetSettingsFile $SETTINGS_DIRECTORY_PATH\$filename) {
+        Write-Host "$filenamWithoutExtension" -ForegroundColor "Green" -NoNewLine
+        Write-Host " <-- current $TARGET_SETTING_FILENAME" -foregroundcolor "White"
+    } else {
+        Write-Host $filenamWithoutExtension -ForegroundColor "Red"
+    }
 }
 
 function printListOfeSettingsFilenames {
